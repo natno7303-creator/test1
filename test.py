39f035ba0e08839083d14d0a1cccfc59cda6b2b4
natno7303-creator <natno7303@gmail.com>
2026-08-24 14:52:52 +0800

    first

---
+++ test.py
@@ -0,0 +1,35 @@
+print("hellow world")
+
+print("1+2")
+
+print(1+2)
+
+print("1 + 2 =",1+2)
+
+message=("zcc")
+print(message)
+
+age=22
+print(message,age)
+
+next_age=age + 1
+print(next_age)
+
+age=age+1
+print(age)
+
+fruits=["苹果","香蕉"]
+print(fruits[1])
+
+fruits.append("西瓜")
+print(fruits)
+
+point=(10,20)
+print(point[0])
+#point[0]=30  元组不能修改
+
+user = {"name1":"zcc","age1":22}
+print(user["name1"])
+user["age1"]=19
+user["Gender"]="xiaozhang"
+print(user)
